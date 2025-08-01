d07a8e62e7c9d480da3108622cb3bd08712c3366
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2025-08-01 10:37:10 +0100

    tests: improve organisation

--- tests/testthat/test-eq5d5l.R
+++ tests/testthat/test-eq5d5l.R
@@ -17,13 +17,6 @@ test_that("as_eq5d5l works", {
     # class is correct
     expect_s3_class(out, c("EQ5D5L", "EQ5D", "tbl_df", "tbl", "data.frame"), exact = TRUE)
 
-    # class dropped/kept as expected
-    tmp <- rbind(out[1:2, ], out[2:3, ])
-    expect_false(inherits(tmp, "EQ5D"))
-    tmp <- rbind(out[1:2, ], out[3:4, ])
-    expect_s3_class(tmp, "EQ5D5L")
-    expect_identical(tmp, out[1:4, ])
-
     # attributes are as expected
     expect_identical(attr(out, "surveyID"), "surveyID")
     expect_identical(attr(out, "respondentID"), "respondentID")
@@ -65,6 +58,13 @@ test_that("EQ5D5L maintain and drop class appropriately", {
 
     # row selection maintains class
     expect_s3_class(out[1:10, ], "EQ5D5L")
+
+    # class dropped/kept as expected
+    tmp <- rbind(out[1:2, ], out[2:3, ])
+    expect_false(inherits(tmp, "EQ5D"))
+    tmp <- rbind(out[1:2, ], out[3:4, ])
+    expect_s3_class(tmp, "EQ5D5L")
+    expect_identical(tmp, out[1:4, ])
 
     # column selection may drop class if necessary columns are missing
     expect_s3_class(out[, -(3:4)], "EQ5D5L")
@@ -118,103 +118,7 @@ test_that("Adding incorrect values to an EQ5D5L object will error", {
     expect_error(tmp[3, 5] <- 6L)
 })
 
-test_that("calculate_utility works as expected (non-DSU type)", {
 
-    # setup
-    dat <- transform(EQ5D5L_surveys, surveyID = as.factor(surveyID))
-    out <- as_eq5d5l(
-        dat,
-        surveyID = "surveyID",
-        respondentID = "respondentID",
-        mobility = "mobility",
-        self_care = "self_care",
-        usual = "usual",
-        pain = "pain",
-        anxiety = "anxiety",
-        vas = "vas"
-    )
-
-    # matches eq5d direct calculation
-    dat <- subset(out, surveyID == "survey01")
-    tmp <- calculate_utility(dat, type = "VT", country = c("Germany", "France"))
-
-    # correct class
-    expect_s3_class(tmp, "utility")
-
-    dat2 <- subset(dat, select = c(mobility, self_care, usual, pain, anxiety))
-    names(dat2) <- c("MO", "SC", "UA", "PD", "AD")
-    gutil <- eq5d::eq5d(dat2, version = "5L", type = "VT", country = "Germany")
-    futil <- eq5d::eq5d(dat2, version = "5L", type = "VT", country = "France")
-    tmp2 <- data.frame(
-        respondentID = dat[[2]],
-        .utility_country = c(
-            rep_len("Germany", length(dat2[[1]])),
-            rep_len("France", length(dat2[[1]]))
-        ),
-        .value = c(gutil, futil)
-    )
-    tmp2 <- tmp2[do.call(order, .subset(tmp2)), ]
-    tmp <- tmp[do.call(order, .subset(tmp)), ]
-    rownames(tmp2) <- NULL
-    expect_identical(
-        subset(tmp, select = c(respondentID, .utility_country, .value)),
-        tmp2
-    )
-
-    # correct columns
-    expect_named(
-        tmp,
-        c("respondentID", "surveyID", ".utility_country", ".utility_type", ".value")
-    )
-})
-
-test_that("calculate_utility works as expected (DSU type)", {
-
-    # setup
-    dat <- transform(EQ5D5L_surveys, surveyID = as.factor(surveyID))
-    out <- as_eq5d5l(
-        dat,
-        surveyID = "surveyID",
-        respondentID = "respondentID",
-        mobility = "mobility",
-        self_care = "self_care",
-        usual = "usual",
-        pain = "pain",
-        anxiety = "anxiety",
-        vas = "vas"
-    )
-
-    # matches eq5d direct calculation
-    dat <- subset(out, surveyID == "survey01")
-    tmp <- calculate_utility(dat, type = "DSU", country = "UK", age = "age", sex = "sex")
-
-    # correct class
-    expect_s3_class(tmp, "utility")
-
-    dat2 <- subset(dat, select = c(mobility, self_care, usual, pain, anxiety, sex, age))
-    names(dat2) <- c("MO", "SC", "UA", "PD", "AD", "sex", "age")
-    uutil <- eq5d::eq5d(dat2, version = "5L", type = "DSU", country = "UK", age = "age", sex = "sex") # nolint: line_length_linter.
-
-    tmp2 <- data.frame(
-        respondentID = dat[[2]],
-        .utility_country = c(rep_len("UK", length(dat2[[1]]))),
-        .value = uutil
-    )
-    tmp2 <- tmp2[do.call(order, .subset(tmp2)), ]
-    tmp <- tmp[do.call(order, .subset(tmp)), ]
-    row.names(tmp2) <- NULL
-    expect_identical(
-        .subset(tmp, c("respondentID", ".utility_country", ".value")),
-        .subset(tmp2, c("respondentID", ".utility_country", ".value"))
-    )
-
-    # correct columns
-    expect_named(
-        tmp,
-        c("respondentID", "surveyID", ".utility_country", ".utility_type", ".value")
-    )
-
-})
 
 test_that("validation works", {
     dat <- EQ5D5L_surveys

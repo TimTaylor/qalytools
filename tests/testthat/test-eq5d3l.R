bcb6344cbc9dca119878a384d2a83632e64714f2
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2025-08-01 10:15:52 +0100

    tests: make all  tests self contained

--- tests/testthat/test-eq5d3l.R
+++ tests/testthat/test-eq5d3l.R
@@ -1,31 +1,26 @@
-dat <- data.table::copy(eq5d3l_example)
-data.table::setnames(
-    dat,
-    old = c("MO", "SC", "UA", "PD", "AD", "time"),
-    new = c("mobility", "self_care", "usual", "pain", "anxiety", "time_index")
-)
-
-out <- as_eq5d3l(dat,
-                 respondentID = "respondentID",
-                 surveyID = "surveyID",
-                 mobility = "mobility",
-                 self_care = "self_care",
-                 usual = "usual",
-                 pain = "pain",
-                 anxiety = "anxiety",
-                 vas = "vas")
-
 test_that("as_eq5d3l works", {
+
+    # setup
+    dat <- data.table::setnames(
+        data.table::copy(eq5d3l_example),
+        old = c("MO", "SC", "UA", "PD", "AD", "time"),
+        new = c("mobility", "self_care", "usual", "pain", "anxiety", "time_index")
+    )
+
+    out <- as_eq5d3l(
+        dat,
+        respondentID = "respondentID",
+        surveyID = "surveyID",
+        mobility = "mobility",
+        self_care = "self_care",
+        usual = "usual",
+        pain = "pain",
+        anxiety = "anxiety",
+        vas = "vas"
+    )
 
     # class is correct
     expect_s3_class(out, c("EQ5D3L", "EQ5D", "tbl_df", "tbl", "data.frame"), exact = TRUE)
-
-    # class dropped/kept as expected
-    tmp <- rbind(out[1:2, ], out[2:3, ])
-    expect_false(inherits(tmp, "EQ5D"))
-    tmp <- rbind(out[1:2, ], out[3:4, ])
-    expect_s3_class(tmp, "EQ5D3L")
-    expect_identical(tmp, out[1:4, ])
 
     # attributes are as expected
     expect_identical(attr(out, "surveyID"), "surveyID")
@@ -53,8 +48,34 @@ test_that("as_eq5d3l works", {
 
 test_that("EQ5D3L maintains class and names appropriately", {
 
+    # setup
+    dat <- data.table::setnames(
+        data.table::copy(eq5d3l_example),
+        old = c("MO", "SC", "UA", "PD", "AD", "time"),
+        new = c("mobility", "self_care", "usual", "pain", "anxiety", "time_index")
+    )
+
+    out <- as_eq5d3l(
+        dat,
+        respondentID = "respondentID",
+        surveyID = "surveyID",
+        mobility = "mobility",
+        self_care = "self_care",
+        usual = "usual",
+        pain = "pain",
+        anxiety = "anxiety",
+        vas = "vas"
+    )
+
     # row selection maintains class
     expect_s3_class(out[1:10, ], "EQ5D3L")
+
+    # class dropped/kept as expected
+    tmp <- rbind(out[1:2, ], out[2:3, ])
+    expect_false(inherits(tmp, "EQ5D"))
+    tmp <- rbind(out[1:2, ], out[3:4, ])
+    expect_s3_class(tmp, "EQ5D3L")
+    expect_identical(tmp, out[1:4, ])
 
     # column selection may drop class if necessary columns are missing
     expect_s3_class(out[, -6], "EQ5D3L")
@@ -83,15 +104,53 @@ test_that("EQ5D3L maintains class and names appropriately", {
 })
 
 test_that("Adding incorrect values to an EQ5D3L object will error", {
-    tmp <- out
-    tmp[3, 3] <- 3.0
-    expect_s3_class(out, "EQ5D3L")
-    expect_error(tmp[3, 4] <- 4.5)
-    expect_error(tmp[3, 4] <- "bob")
-    expect_error(tmp[3, 4] <- 6L)
+
+    # setup
+    dat <- data.table::setnames(
+        data.table::copy(eq5d3l_example),
+        old = c("MO", "SC", "UA", "PD", "AD", "time"),
+        new = c("mobility", "self_care", "usual", "pain", "anxiety", "time_index")
+    )
+
+    out <- as_eq5d3l(
+        dat,
+        respondentID = "respondentID",
+        surveyID = "surveyID",
+        mobility = "mobility",
+        self_care = "self_care",
+        usual = "usual",
+        pain = "pain",
+        anxiety = "anxiety",
+        vas = "vas"
+    )
+
+    out[3, 3] <- 3.0
+    expect_error(out[3, 4] <- 4.5)
+    expect_error(out[3, 4] <- "bob")
+    expect_error(out[3, 4] <- 6L)
 })
 
 test_that("calculate_utility works as expected", {
+
+    # setup
+    dat <- data.table::setnames(
+        data.table::copy(eq5d3l_example),
+        old = c("MO", "SC", "UA", "PD", "AD", "time"),
+        new = c("mobility", "self_care", "usual", "pain", "anxiety", "time_index")
+    )
+
+    out <- as_eq5d3l(
+        dat,
+        respondentID = "respondentID",
+        surveyID = "surveyID",
+        mobility = "mobility",
+        self_care = "self_care",
+        usual = "usual",
+        pain = "pain",
+        anxiety = "anxiety",
+        vas = "vas"
+    )
+
     # matches eq5d direct calculation
     tmp <- calculate_utility(out, type = "TTO", country = c("Germany", "France"))
     dat2 <- subset(dat, select = c(mobility, self_care, usual, pain, anxiety))
